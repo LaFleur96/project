643415ecdd555ffad1833a744b711c56e176c487
Jekson <example@gmail.com>
2020-03-27 13:07:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// console.log(4/0);
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// }
+
+// console.log(persone);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[0])
+
+// alert("Hello, world!");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = +prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(++incr);
+console.log(--decr);
+
+let isChcked = true,
+    isClose = false;
+
+console.log(isChcked && isClose);
+console.log(isChcked || isClose);
